c12f985214c9eeca27bec8acd6846ad3ce84e845
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-10-03 22:16:40 +0300

    рішення завдання 2 та заготовки завдання 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+('user strict');
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    galleryItem => `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markup);
+let selectedImg = 0;
+
+function handlerClick(event) {
+  event.preventDefault();
+  return (selectedImg = event.target.dataset.source);
+}
+galleryEl.addEventListener('click', handlerClick);
+
+galleryEl.onclick = () => {
+  basicLightbox.create(`<img width="1400" height="900" src="${selectedImg}">`).show();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+('user strict');
 
+const galleryEl = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    galleryItem => `<div class="gallery__item">
+  <a class="gallery__item" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markup);
 console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
